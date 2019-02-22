c849436f112c33a41aac4e7b7ea841d3bc91e9b3
EMarcel92 <emmanuel.marcel@axa.fr>
2019-02-22 16:12:38 +0100

    Suppression des classes superflues (Affichage, CodeSecret). Javadoc de toutes les méthodes. Main nettoyé. PlisMoins fonctionne dans les 2 modes.

--- src/main/java/manu/tuto/ParametresDuJeu.java
+++ src/main/java/manu/tuto/ParametresDuJeu.java
@@ -1,10 +1,20 @@
 package manu.tuto;
 
 public class ParametresDuJeu {
-    public static int LONGUEUR_CODE_SECRET = 3;   //Nombre de symboles du code secret
-    public static int NB_MAX_SYMBOLES= 5;      // Nombre de valeurs possibles pour un symbole
-    public static int NB_MAX_ESSAIS = 3;       //Nombre d'essais max possibles pour trouver la combinaison
-    public static boolean MODE_DEV = true;  // Mode développeur permettant d'afficher de code secret avant de démarrer la partie
+    public static int LONGUEUR_CODE_SECRET;   //Nombre de symboles du code secret
+    public static int NB_MAX_SYMBOLES;      // Nombre de valeurs possibles pour un symbole, de 0 à (NB_MAX_SYMBOLES - 1)
+    public static int NB_MAX_ESSAIS;       //Nombre d'essais max possibles pour trouver la combinaison
+    public static boolean MODE_DEV;  // Mode développeur permettant d'afficher de code secret avant de démarrer la partie
+
+
+    public static void intialiserLesParametres(){
+        //TODO lire le fichier de config
+
+        LONGUEUR_CODE_SECRET = 3;
+        NB_MAX_SYMBOLES = 3;
+        NB_MAX_ESSAIS = 3;
+        MODE_DEV = true;
+    }
 
 
 //TODO controler longueur max
